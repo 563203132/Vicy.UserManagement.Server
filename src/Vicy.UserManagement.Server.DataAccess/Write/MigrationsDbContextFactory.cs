5f575ea30e36cfd6f3a16594c18a3f4fd4040514
Vic Li <563203132@qq.com>
2017-03-23 21:08:03 +0800

    Fix Issue: Update database using command will throw exception. Exception detail information: The target context is not constructible.

---
+++ src/Vicy.UserManagement.Server.DataAccess/Write/MigrationsDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.Infrastructure;
+
+namespace Vicy.UserManagement.Server.DataAccess.Write
+{
+    public class MigrationsDbContextFactory : IDbContextFactory<WriteDbContext>
+    {
+        public WriteDbContext Create()
+        {
+            // The dependencies are not necessary for db migration.
+            return new WriteDbContext(null, null);
+        }
+    }
+}
